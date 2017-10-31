0dcd42e13409de24466aadd9b8339872840f3e89
maniafish <mania_fish@163.com>
2017-10-31 12:15:03 +0800

    build router

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+go build -v
+if [ $? -ne 0 ];then
+    exit 1
+fi
+
+# try start beeme for build router
+./beeme &
+sleep 1
+PID=`ps ax | awk '$NF=="./beeme"{print $1; exit}'`
+if [ -z "PID" ];then
+    echo "start beeme failed"
+    exit 1
+fi
+
+kill ${PID}

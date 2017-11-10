39d97eaf3b8acf7480ea3fadff707e9ae52d0471
maniafish <mania_fish@163.com>
2017-11-10 22:16:25 +0800

    golint -> build.sh lint

--- build.sh
+++ build.sh
@@ -1,5 +1,20 @@
 #!/bin/sh
 
+function go_lint(){
+    for pkg in $(go list ./... | grep -v /vendor/)
+    do
+        golint -set_exit_status $pkg
+        if [ $? -ne 0 ]; then
+            exit 1
+        fi
+    done
+}
+
+if [ "$1" == "lint" ]; then
+    go_lint
+    exit 0
+fi
+
 go build -v
 if [ $? -ne 0 ];then
     exit 1

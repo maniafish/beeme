ec57574ceee40537b3f1c5791447d44f2c93a6eb
maniafish <mania_fish@163.com>
2017-11-10 21:21:06 +0800

    add args

--- conf/config.go
+++ conf/config.go
@@ -24,15 +24,15 @@ var Config *MainConfig
 
 func init() {
 	Config = &MainConfig{}
-	file := filepath.Join("conf", "config.toml")
-	path, _ := filepath.Abs(file)
-	// 路径无效
-	if len(filepath.SplitList(path)) <= 2 {
+	var file string
+	// 无配置
+	if len(os.Args) < 2 {
 		_, thisFilePath, _, _ := runtime.Caller(0)
 		file = filepath.Join(filepath.Dir(thisFilePath), "config.toml")
+	} else {
+		file = os.Args[1]
 	}
 
-	logs.Info("config path: %v", path)
 	if _, err := toml.DecodeFile(file, Config); err != nil {
 		logs.Error("Parse config.toml Failed: %v", err)
 		os.Exit(1)

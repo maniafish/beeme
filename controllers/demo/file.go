e65ae5be1bfb9fdf63c578c9571554e33a221198
maniafish <mania_fish@163.com>
2018-11-30 11:31:08 +0800

    v1.1.0 修复文件上传功能

--- controllers/demo/file.go
+++ controllers/demo/file.go
@@ -2,10 +2,8 @@ package demo
 
 import (
 	"beeme/controllers"
-	"fmt"
 	"os"
 	"path"
-	"time"
 
 	"github.com/astaxie/beego"
 )
@@ -65,10 +63,11 @@ func (u *FileController) Post() {
 */
 
 // PostFlow 处理流式上传文件，存入本地
-// @router /flow [post]
+// @router /flow/:fname [post]
 func (u *FileController) PostFlow() {
-	tofile := path.Join(beego.AppConfig.String("apps::FileDir"), fmt.Sprintf("%v", time.Now().Unix()))
-	tf, err := os.OpenFile(tofile, os.O_WRONLY|os.O_CREATE, 0666)
+	fname := u.GetString(":fname")
+	tofile := path.Join(beego.AppConfig.String("apps::FileDir"), fname)
+	tf, err := os.OpenFile(tofile, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0666)
 	if err != nil {
 		u.Logger.Infof("OpenFile err: %v", err)
 		u.ServeJSON(200, &FileResponse{-1, "internal error"})
